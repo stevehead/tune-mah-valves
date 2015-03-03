1dbd819a103c32926ee2b648413383c59c02a8ea
Steve Johnson <sjohnson0684@gmail.com>
2015-03-03 16:26:39 -0500

    Initial Wavelength coding.

--- src/com/stevehead/music/physics/Wavelength.java
+++ src/com/stevehead/music/physics/Wavelength.java
@@ -1,5 +1,64 @@
 package com.stevehead.music.physics;
 
-public class Wavelength {
+import com.stevehead.music.Note;
 
+/**
+ * Wavelength is a class that represents the length of a wave in physics. More
+ * specifically a soundwave for music calculations.
+ * 
+ * @author	Steve Johnson
+ */
+public class Wavelength {
+	
+	/**
+	 * The length of the wavelength of middle C. Based on the approximation of
+	 * C's being in powers of 2 feet (1ft, 2ft, 4ft, 8ft, etc.). Since the
+	 * speed of sound is not constant and varies with temperature, air density,
+	 * etc., a standard must be set, much like how a tuning standard must be set
+	 * for A.
+	 */
+	public static final double MIDDLE_C_WAVELENGTH = 24;
+	
+	/**
+	 * The wavelength in inches.
+	 */
+	public double inches;
+	
+	/**
+	 * Constructor for inches input.
+	 * 
+	 * @param inches		the length of the wavelength in inches
+	 */
+	public Wavelength(double inches) {
+		this.inches = inches;
+	}
+	
+	/**
+	 * Converts the wavelength to a frequency.
+	 * 
+	 * @return		the frequency of the wavelength
+	 */
+	public Frequency toFrequency() {
+		// TODO: code logic
+		return new Frequency();
+	}
+	
+	/**
+	 * Converts the wavelength to a note.
+	 * 
+	 * @return		the musical note with this wavelength
+	 */
+	public Note toNote() {
+		// TODO: code logic
+		return new Note();
+	}
+	
+	/**
+	 * The string representation of a wavelength.
+	 * 
+	 * @return		the length of the wavelength as a string
+	 */
+	public String toString() {
+		return Double.toString(inches) + "in";
+	}
 }
